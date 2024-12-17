553fd2cf42ee0e8fceea1c2ff11052ea1341048f
GuilleFerru <guillelf@gmail.com>
2024-12-17 14:00:32 -0300

    week6.5 done

--- week6.5.js
+++ week6.5.js
@@ -31,25 +31,31 @@
 
 function formingMagicSquare(s) {
     // Write your code here
-    const n = s.length;
-    const magicNumber = (n * ((n * n) + 1)) / 2
+    const magicOnes = [
+        [8, 1, 6, 3, 5, 7, 4, 9, 2],
+        [6, 7, 2, 1, 5, 9, 8, 3, 4],
+        [2, 9, 4, 7, 5, 3, 6, 1, 8],
+        [4, 3, 8, 9, 5, 1, 2, 7, 6],
+        [6, 1, 8, 7, 5, 3, 2, 9, 4],
+        [8, 3, 4, 1, 5, 9, 6, 7, 2],
+        [4, 9, 2, 3, 5, 7, 8, 1, 6],
+        [2, 7, 6, 9, 5, 1, 4, 3, 8]
+    ];
+    const flatArray = s.flat()
 
+    const totalCosts = []
 
-    //const red = s.reduce((acc, curr) => Number(acc)+Number(curr))
-    const y = s[1][1]
-
-
-
-    for (let i = 0; i < s.length; i++) {
-        for (let j = 0; j < s[i].length; j++) {
-            if (s[0][1] !== (y - 1)){
-                
-            }
+    for (let i = 0; i < magicOnes.length; i++) {
+        let count = 0;
+        for (let j = 0; j < 9; j++) {
+            const flatVal = flatArray[j]
+            const magicVal = magicOnes[i][j]
+            count += Math.abs(flatVal - magicVal)
         }
+        totalCosts.push(count)
     }
 
-    return 'red'
-
+    return Math.min(...totalCosts)
 }
 
 function main() {
@@ -61,7 +67,8 @@ function main() {
     //     s[i] = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
     // }
 
-    const result = formingMagicSquare([[4, 9, 2], [3, 5, 7], [8, 1, 5]]);
+    const result = formingMagicSquare([[1, 3, 8], [6, 4, 1], [2, 6, 5]]);
+    //const result = formingMagicSquare([[4, 9, 2], [3, 5, 7], [8, 1, 5]]);
     console.log(result)
     //ws.write(result + '\n');
 
